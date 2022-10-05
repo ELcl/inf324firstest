0a36f316a871ec89ad134a6ce7105c5eae8289fa
Efr@ <ELcl@users.noreply.github.com>
2022-10-04 20:27:13 -0400

    Add files via upload

---
+++ inf324/index.php
@@ -0,0 +1,9 @@
+<?php
+		$nivel1="style.css";
+		include "cabecera.php";
+		include "menu.php";
+?>
+		La Facultad de Ciencias Puras y Naturales se encuentra ubicada en el monoblock central y calle 27 de Cota Cota. 
+<?php
+		include "pie.php";
+?>
---
+++ inf324/qmc/index.php
@@ -0,0 +1,17 @@
+
+		<?php
+		$nivel1= "../style.css";
+		include "../cabecera.php";
+		?>
+		<div id="menu">
+			<td><a href="../inf/index.php">Informatica</a></td>
+			<td><a href="#">Matem&aacutetica</a></td>
+			<td><a href="../qmc/index.php">Qu&iacutemica</a></td>
+		</div>
+
+		<div id="contenido" >
+			Qu&iacutemica una carrera bonita
+
+		<?php
+		include "../pie.php";
+		?>
