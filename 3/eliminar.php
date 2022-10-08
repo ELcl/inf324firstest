51ecc7c468dff1a65760568c4e21425a93f0b68f
Efr@ <ELcl@users.noreply.github.com>
2022-10-08 12:42:01 -0400

    Delete eliminar.php

--- 3/eliminar.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$ci=$_GET["ci"];
-	include "conexion.inc.php";
-	$sql="delete from alumno where ci='$ci'";
-	//echo $sql;
-	$resultado=mysqli_query($con, $sql);
-	header("Location: inicial.php");
-?>
